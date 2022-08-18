abb5c7291447c1df60a6fcfe8731256a58e86369
unknown <ssaquict@gmail.com>
2022-08-17 23:31:25 -0600

    primier commit

---
+++ src/vista/main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+
+/**
+ *
+ * @author SistemasU
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cadena;
+        frm_cadena frm = new frm_cadena ();
+        frm.show();
+        
+        
+    }
+
+    
+}
